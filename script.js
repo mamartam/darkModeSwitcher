39f360ef466259dcc673e49497235b66c3661078
mamartam <marta.masiukk@gmail.com>
2024-08-05 11:20:54 +0300

    added script for switcher

---
+++ script.js
@@ -0,0 +1,10 @@
+const SwitchTheme = ()=>{
+    const rootElem = document.documentElement;
+    let dataTheme = rootElem.getAttribute("data-theme"), newTheme;
+    newTheme = (dataTheme ==="light")?"dark":"light";
+
+    rootElem.setAttribute("data-theme", newTheme);
+
+
+}
+document.querySelector("#theme-switcher").addEventListener("click", SwitchTheme);
